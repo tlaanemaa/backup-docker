8b2e3e4633d2ac740a3eed43bb98755dc2e0c774
Taavi <Taavi.laanemaa@gmail.com>
2019-04-25 15:49:29 +0300

    Updated main file

--- src/main.js
+++ src/main.js
@@ -5,44 +5,43 @@ const { getAllContainerConfigs } = require('./modules/utils');
 // Grab command args
 const [operation, ...containerNames] = process.argv.slice(2);
 
+// Helper to print command line args
+// eslint-disable-next-line no-console
+const printUsage = () => console.log('Usage: backup-docker <backup|restore> [<container-name, container-name, ...>]');
+
 // Main backup function
 const backup = async () => {
-  try {
-    const containers = containerNames.length ? containerNames : await getContainers();
-    // eslint-disable-next-line no-console
-    console.log('Backing up the following container inspections:', containers.join(', '));
-    return await Promise.all(containers.map(backupContainer));
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e.message);
-    return null;
-  }
+  const containers = containerNames.length ? containerNames : await getContainers();
+  // eslint-disable-next-line no-console
+  console.log('Backing up the following container inspections:', containers.join(', '));
+  return Promise.all(containers.map(backupContainer));
 };
 
 // Main restore function
 const restore = async () => {
-  try {
-    const containers = containerNames.length ? containerNames : await getAllContainerConfigs();
-    // eslint-disable-next-line no-console
-    console.log('Restoring the following containers:', containers.join(', '));
-    return await Promise.all(containers.map(restoreContainer));
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e.message);
-    return null;
-  }
+  const containers = containerNames.length ? containerNames : await getAllContainerConfigs();
+  // eslint-disable-next-line no-console
+  console.log('Restoring the following containers:', containers.join(', '));
+  return Promise.all(containers.map(restoreContainer));
 };
 
 // Run the operation requested
-switch (operation) {
-  case 'backup':
-    backup();
-    break;
+try {
+  switch (operation) {
+    case 'backup':
+      backup();
+      break;
 
-  case 'restore':
-    restore();
-    break;
+    case 'restore':
+      restore();
+      break;
 
-  default:
-    throw new Error('Invalid operation!');
+    default:
+      // eslint-disable-next-line no-console
+      console.error('Invalid operation!');
+      printUsage();
+  }
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.error(e.message);
 }
