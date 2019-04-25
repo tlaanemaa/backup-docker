ac4b66fd58e59c2639ce1c6289298f7955d31a8a
Taavi <Taavi.laanemaa@gmail.com>
2019-04-25 17:45:11 +0300

    Made main functions not fail on one container error

--- src/main.js
+++ src/main.js
@@ -12,34 +12,47 @@ const printUsage = () => console.log('Usage: backup-docker <backup|restore> [<co
 // Main backup function
 const backup = async () => {
   const containers = containerNames.length ? containerNames : await getContainers();
-  return Promise.all(containers.map(backupContainer));
+  return Promise.all(containers.map(async (container) => {
+    try {
+      await backupContainer(container);
+      return true;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+      return false;
+    }
+  }));
 };
 
 // Main restore function
 const restore = async () => {
   const containers = containerNames.length ? containerNames : await getAllContainerConfigs();
-  return Promise.all(containers.map(restoreContainer));
+  return Promise.all(containers.map(async (container) => {
+    try {
+      await restoreContainer(container);
+      return true;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+      return false;
+    }
+  }));
 };
 
 // Run the operation requested in an async wrapper
 (async () => {
-  try {
-    switch (operation) {
-      case 'backup':
-        await backup();
-        break;
+  switch (operation) {
+    case 'backup':
+      await backup();
+      break;
 
-      case 'restore':
-        await restore();
-        break;
+    case 'restore':
+      await restore();
+      break;
 
-      default:
-        // eslint-disable-next-line no-console
-        console.error('Invalid operation!');
-        printUsage();
-    }
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e.message);
+    default:
+      // eslint-disable-next-line no-console
+      console.error('Invalid operation!');
+      printUsage();
   }
 })();
