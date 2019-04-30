d2b6a77b759542f1620bfa5c631fa0314934f7a4
Taavi <Taavi.laanemaa@gmail.com>
2019-04-30 21:31:11 +0300

    Fixed tests

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -28,7 +28,6 @@ const mockInspectFile = `
 
 fs.writeFile = jest.fn().mockImplementation((x, y, callback) => callback());
 fs.readFile = jest.fn().mockImplementation((x, callback) => callback(null, mockInspectFile));
-fs.readdir = jest.fn().mockImplementation((x, callback) => callback(null, ['a.json', 'b.json', 'c.json']));
-fs.readdirSync = jest.fn().mockImplementation((_, ext) => [`a${ext}`, `a${ext}`, `a${ext}`]);
+fs.readdirSync = jest.fn().mockReturnValue(['a.json', 'b.json', 'c.json']);
 
 module.exports = fs;
