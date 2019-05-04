4fab836f27baa0be130dbf84b715e2f87413bee6
Taavi <Taavi.laanemaa@gmail.com>
2019-05-04 18:05:44 +0300

    Moved global error handling to run.js

--- bin/run.js
+++ bin/run.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-require('..')();
+
+const logAndExit = (err) => {
+  // eslint-disable-next-line no-console
+  console.error(err instanceof Error ? err.message : err);
+  process.exit(1);
+};
+
+// Global error handlers
+process.on('uncaughtException', logAndExit);
+process.on('unhandledRejection', logAndExit);
+
+(async () => {
+  // eslint-disable-next-line global-require
+  await require('..')();
+
+  // Safeguard against hanging application
+  process.exit(0);
+})();
--- src/modules/globalErrorHandlers.js
+++
@@ -1,8 +0,0 @@
-const handleError = (err) => {
-  // eslint-disable-next-line no-console
-  console.error(err instanceof Error ? err.message : err);
-  process.exit(1);
-};
-
-process.on('uncaughtException', handleError);
-process.on('unhandledRejection', handleError);
